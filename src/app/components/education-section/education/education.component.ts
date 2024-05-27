77af46da0f8170490be9eced9af7b05b48e90b87
Matthew Evans <matt29992@gmail.com>
2024-05-27 15:22:27 +0100

    modifying education

--- src/app/components/education-section/education/education.component.ts
+++ src/app/components/education-section/education/education.component.ts
@@ -5,17 +5,19 @@ import { Component, Input, OnInit } from '@angular/core';
   templateUrl: './education.component.html',
   styleUrls: ['./education.component.scss']
 })
+export class EducationComponent implements OnInit {
 
-export class EducationComponent {
+  @Input() educationFieldOfStudy: string = 'Unknown Field';
+  @Input() educationInstitution: string = 'Unknown Institution';
+  @Input() educationGraduationDate: Date = new Date();
+  @Input() educationStartDate: Date = new Date();
+  @Input() educationLevel: string = 'Unknown Level';
+  @Input() educationGrade: string = 'N/A';
+  @Input() educationActivities: string[] = [];
+  @Input() educationTechnologies: { name: string, iconUrl: string }[] = [];
 
-  @Input() educationFieldOfStudy: string = 'Computer Science';
-  @Input() educationInstitution: string = 'open university';
-  @Input() educationGraduationDate: Date = new Date('2020');
-  @Input() educationStartDate: Date = new Date('2016');
-  @Input() educationLevel: string = 'Bachelor';
-  @Input() educationGrade: number = 3.5;
-  @Input() educationTechnologies: string[] = ['Angular', 'TypeScript', 'HTML', 'CSS'];
-
-  constructor() {}
-
+  ngOnInit(): void {
+    this.educationActivities = this.educationActivities.length ? this.educationActivities : [''];
+    this.educationTechnologies = this.educationTechnologies.length ? this.educationTechnologies : [{ name: '', iconUrl: '' }];
+  }
 }
